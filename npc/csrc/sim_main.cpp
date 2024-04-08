f73a4228ccab9c90148125a27824e35be5b4ebf1
oz1010 <uu90909@gmail.com>
2024-04-08 10:51:49 +0800

    feat: add VM_TRACE_VCD macro to compile

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -5,22 +5,29 @@
 #include <memory>
 #include "Vtop.h"
 #include "verilated.h"
-//#include "verilated_fst_c.h"
 
+#if VM_TRACE_VCD
+#include "verilated_vcd_c.h"
+#endif
 
 int main(int argc, char** argv)
 {
-	// Create logs directory in case we have traces to put under it
-	Verilated::mkdir("logs");
-
 	const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 	contextp->debug(0); // Set debug level, 0 is off, 9 is highest
 	contextp->randReset(2); // Randomization reset policy
-	contextp->traceEverOn(true); // Verilator must compute traced signals
 	contextp->commandArgs(argc, argv); // Pass arguments so Verilated code can see them
 	
 	// "TOP" will be hierarchical name of the module.
 	const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+
+#if VM_TRACE_VCD
+	Verilated::mkdir("logs");
+	contextp->traceEverOn(true); // Verilator must compute traced signals
+	const std::unique_ptr<VerilatedVcdC>tfp{new VerilatedVcdC};
+	top->trace(tfp.get(), 99); // Trace 99 levels of hierarchy (or see below)
+	tfp->open("logs/simu_top.vcd");
+	printf("Start trace ...\n");
+#endif
 
 	uint64_t sim_time = 10000;
 	while(contextp->time()<sim_time && !contextp->gotFinish())
@@ -34,10 +41,13 @@ int main(int argc, char** argv)
 		top->eval();
 		// printf("a = %d, b = %d, f = %d, time = %lu\n", a, b, top->f, contextp->time());
 		assert(top->f == (a ^ b));
+
+#if VM_TRACE_VCD
+		tfp->dump(contextp->time());
+#endif
 	}
 
 	top->final();
-	// tfp->close();
 
 	// Coverage analysis
 #if VM_COVERAGE
@@ -45,10 +55,6 @@ int main(int argc, char** argv)
 	contextp->converagep()->write("logs/coverage.dat");
 #endif
 
-	// delete top;
-	// delete contextp;
-	//
-	
 	// Final simulation summary
 	contextp->statsPrintSummary();
 
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -8,11 +8,11 @@ module top
   assign f = a ^ b;
 
   initial begin
-    if ($test$plusargs("trace") != 0) begin
-	  $display("[%0t] Tracing to logs/vlt_dump.vcd...\n", $time);
-	  $dumpfile("logs/vlt_dump.vcd");
-	  $dumpvars();
-	end
+//    if ($test$plusargs("trace") != 0) begin
+//	  $display("[%0t] Tracing to logs/vlt_dump.vcd...\n", $time);
+//	  $dumpfile("logs/vlt_dump.vcd");
+//	  $dumpvars();
+//	end
 	$display("[%0t] Model running...\n", $time);
   end
 
