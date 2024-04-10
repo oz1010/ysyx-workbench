02a3a75f77875b280d15e443b3deaee40b50a447
oz1010 <uu90909@gmail.com>
2024-04-10 20:42:07 +0800

    feat: add example

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,62 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <stdint.h>
-#include <memory>
-#include "Vtop.h"
 #include "verilated.h"
-
-#if VM_TRACE_VCD
 #include "verilated_vcd_c.h"
-#endif
+#include "Vtop.h"
 
-int main(int argc, char** argv)
-{
-	const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-	contextp->debug(0); // Set debug level, 0 is off, 9 is highest
-	contextp->randReset(2); // Randomization reset policy
-	contextp->commandArgs(argc, argv); // Pass arguments so Verilated code can see them
-	
-	// "TOP" will be hierarchical name of the module.
-	const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
 
-#if VM_TRACE_VCD
-	Verilated::mkdir("logs");
-	contextp->traceEverOn(true); // Verilator must compute traced signals
-	const std::unique_ptr<VerilatedVcdC>tfp{new VerilatedVcdC};
-	top->trace(tfp.get(), 99); // Trace 99 levels of hierarchy (or see below)
-	tfp->open("logs/simu_top.vcd");
-	printf("Start trace ...\n");
-#endif
+static Vtop* top;
 
-	uint64_t sim_time = 10000;
-	while(contextp->time()<sim_time && !contextp->gotFinish())
-	{
-		contextp->timeInc(1);
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vtop;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("dump.vcd");
+}
 
-		int a = rand() & 1;		
-		int b = rand() & 1;		
-		top->a = a;
-		top->b = b;
-		top->eval();
-		// printf("a = %d, b = %d, f = %d, time = %lu\n", a, b, top->f, contextp->time());
-		assert(top->f == (a ^ b));
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+}
 
-#if VM_TRACE_VCD
-		tfp->dump(contextp->time());
-#endif
-	}
+int main() {
+  sim_init();
 
-	top->final();
+  top->s=0; top->a=0; top->b=0;  step_and_dump_wave();   // 将s，a和b均初始化为“0”
+                      top->b=1;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，继续保持“0”，
+            top->a=1; top->b=0;  step_and_dump_wave();   // 将a，b分别改为“1”和“0”，s的值不变，
+                      top->b=1;  step_and_dump_wave();   // 将b改为“1”，s和a的值不变，维持10个时间单位
+  top->s=1; top->a=0; top->b=0;  step_and_dump_wave();   // 将s，a，b分别变为“1,0,0”，维持10个时间单位
+                      top->b=1;  step_and_dump_wave();
+            top->a=1; top->b=0;  step_and_dump_wave();
+                      top->b=1;  step_and_dump_wave();
 
-	// Coverage analysis
-#if VM_COVERAGE
-	Verilated::mkdir("logs");
-	contextp->converagep()->write("logs/coverage.dat");
-#endif
-
-	// Final simulation summary
-	contextp->statsPrintSummary();
-
-	return 0;
+  sim_exit();
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,19 +1,7 @@
-module top
-  (
-  input a,
-  input b,
-  output f
-  );
-
-  assign f = a ^ b;
-
-  initial begin
-//    if ($test$plusargs("trace") != 0) begin
-//	  $display("[%0t] Tracing to logs/vlt_dump.vcd...\n", $time);
-//	  $dumpfile("logs/vlt_dump.vcd");
-//	  $dumpvars();
-//	end
-	$display("[%0t] Model running...\n", $time);
-  end
+module top(
+    input   a,b,s,
+    output  y
+    );
+  assign  y = (~s&a)|(s&b);  // 实现电路的逻辑功能。
 
 endmodule
