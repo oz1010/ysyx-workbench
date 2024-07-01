489f9787db7aa50595858edae3baf6dfb76f7c00
oz1010 <uu90909@gmail.com>
2024-07-01 10:05:16 +0800

    feat: replace memmove with memcpy

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -71,14 +71,7 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  int i;
-  unsigned char* d = dst;
-  const unsigned char* s = src;
-
-  for (i=0; i<n; ++i)
-    d[i] = s[i];
-
-  return dst;
+  return memcpy(dst, src, n);
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
