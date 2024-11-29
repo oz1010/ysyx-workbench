ba3bc29581dca26b46f86fd7f5c4f6496b5cbb54
oz1010 <uu90909@gmail.com>
2024-11-29 11:19:04 +0800

    feat: npc add batch mode

--- npc/csrc/monitor/monitor.cpp
+++ npc/csrc/monitor/monitor.cpp
@@ -34,6 +34,7 @@ void init_difftest(char *ref_so_file, long img_size, int port);
 // void init_device();
 void init_sdb();
 void init_disasm(const char *triple);
+void sdb_set_batch_mode();
 
 FILE *log_fp = NULL;
 static char def_img_file[] = "npc/build/test/addi/case.bin";
@@ -66,8 +67,6 @@ static void welcome()
 // #ifndef CONFIG_TARGET_AM
 // #include <getopt.h>
 
-// void sdb_set_batch_mode();
-
 // static char *log_file = NULL;
 // static char *diff_so_file = NULL;
 // static char *img_file = NULL;
@@ -98,6 +97,7 @@ static void welcome()
 static int parse_args(int argc, char *argv[])
 {
     const struct option table[] = {
+        {"batch", no_argument, NULL, 'b'},
         {"port", required_argument, NULL, 'p'},
         {"log", required_argument, NULL, 'l'},
         {"diff", required_argument, NULL, 'd'},
@@ -105,10 +105,13 @@ static int parse_args(int argc, char *argv[])
         {0, 0, NULL, 0},
     };
     int o;
-    while ((o = getopt_long(argc, argv, "-hl:d:p:", table, NULL)) != -1)
+    while ((o = getopt_long(argc, argv, "-hl:d:p:b", table, NULL)) != -1)
     {
         switch (o)
         {
+            case 'b':
+                sdb_set_batch_mode();
+                break;
             case 'p':
                 sscanf(optarg, "%d", &cfg_difftest_port);
                 break;
@@ -123,6 +126,7 @@ static int parse_args(int argc, char *argv[])
                 return 0;
             default:
                 printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+                printf("\t-b,--batch              run with batch mode\n");
                 printf("\t-l,--log=FILE           output log to FILE\n");
                 printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
                 printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
