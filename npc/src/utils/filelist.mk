c5620ef2ae0ca92b99a4f11b23e1d29660bab10f
oz1010 <uu90909@gmail.com>
2024-12-11 16:17:46 +0800

    feat: add nemu-diff

---
+++ npc/src/utils/filelist.mk
@@ -0,0 +1,22 @@
+#***************************************************************************************
+# Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+#
+# NEMU is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+#
+# See the Mulan PSL v2 for more details.
+#**************************************************************************************/
+
+ifneq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
+CCSRC = src/utils/disasm.cc
+# CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
+CXXFLAGS += -I/usr/lib/llvm-14/include -fno-exceptions -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -fPIE
+# CXXFLAGS += -D__STDC_FORMAT_MACROS
+LDFLAGS += $(shell llvm-config --libs)
+endif
